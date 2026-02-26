48f9da2d47e14bfb1fa1df4b2400e8c413afbe92
Anant2709 <anant04@terpmail.umd.edu>
2026-02-26 17:08:55 -0500

    Adding Tab 1 and changing the container size and colors and adding icons

--- js/app.js
+++ js/app.js
@@ -1,30 +1,26 @@
-function openTab(tabName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
+function openTab(tabName, elmnt) {
   var i, tabcontent, tablinks;
+
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Remove the background color of all tablinks/buttons
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
+    tablinks[i].classList.remove("active");
   }
 
-  // Show the specific tab content
-  const targetTab = document.getElementById(tabName);
+  var targetTab = document.getElementById(tabName);
   if (targetTab) {
-    targetTab.style.display = "block";
+    targetTab.style.display = (tabName === "Text") ? "flex" : "block";
   }
 
-  // Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
+  elmnt.classList.add("active");
 }
 
-// Get the element with id="defaultOpen" and click on it
 document.addEventListener("DOMContentLoaded", function() {
-  const defaultTab = document.getElementById("defaultOpen");
+  var defaultTab = document.getElementById("defaultOpen");
   if (defaultTab) {
     defaultTab.click();
   }
